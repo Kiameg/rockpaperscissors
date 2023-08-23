f657a84d5651d2b6cd257431e017b883a844492a
Kiameg <kiax1262@stud.kea.dk>
2023-08-23 22:19:56 +0200

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorButton = document.querySelector(".scissors");
+const playerOne = document.querySelector("#player1");
+const playerTwo = document.querySelector("#player2");
+const win = document.querySelector("#win");
+const lose = document.querySelector("#lose");
+const draw = document.querySelector("#draw");
+const hidden = document.querySelector(".hidden");
+const animation = document.querySelector("shake");
